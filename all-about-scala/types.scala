21ffa7ae1387047058bdb72aabd043192ac65c01
Amol Dharmadhikari <adharmad@ionate.io>
2022-01-22 18:21:05 -0800

    Some example programs from allaboutscala tutorial

---
+++ all-about-scala/types.scala
@@ -0,0 +1,23 @@
+// Types
+
+val donutsBought: Int = 5
+val bigNumberOfDonuts: Long = 100000000L
+val smallNumberOfDonuts: Short = 1
+val priceOfDonut: Double = 2.50
+val donutPrice: Float = 2.50f
+val donutStoreName: String = "allaboutscala Donut Store"
+val donutByte: Byte = 0xa
+val donutFirstLetter: Char = 'D'
+val nothing: Unit = ()
+
+
+
+println ("donutsBought = " + donutsBought)
+println ("bigNumberOfDonuts = " + bigNumberOfDonuts)
+println ("smallNumberOfDonuts = " + smallNumberOfDonuts)
+println ("priceOfDonut = " + priceOfDonut)
+println ("donutPrice = " + donutPrice)
+println ("donutStoreName = " + donutStoreName)
+println ("donutByte = " + donutByte)
+println ("donutFirstLetter = " + donutFirstLetter)
+println ("nothing = " + nothing)
